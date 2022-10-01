8be00e396c4f2af7883cdf5965bcd94ecd483364
datispham <datistpham@gmail.com>
2022-10-01 16:03:03 +0700

    upload

--- db/init.js
+++ db/init.js
@@ -1,13 +1,19 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const database = 'tricklor'; // REPLACE WITH YOUR DB NAME
-const uri = process.env.URL_CONNECT_MONGO || `mongodb+srv://thai626:thai626vippro@cluster0.n1dlm60.mongodb.net/${database}?retryWrites=true&w=majority`;
-const connectMongo= async ()=> {
+const database = "tricklor"; // REPLACE WITH YOUR DB NAME
+const uri =
+  process.env.URL_CONNECT_MONGO ||
+  `mongodb+srv://thai626v:thai626v@cluster0.2bu7ahg.mongodb.net/${database}?retryWrites=true&w=majority`;
+const connectMongo = async () => {
   try {
-     mongoose.connect(uri ,{ useNewUrlParser: true, useUnifiedTopology: true }, ()=> console.log("Mongoose is connected"))
+    mongoose.connect(
+      uri,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => console.log("Mongoose is connected")
+    );
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
-export const dbconnection= mongoose.connection
-export default connectMongo
+};
+export const dbconnection = mongoose.connection;
+export default connectMongo;
